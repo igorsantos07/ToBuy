562df44424ecb7ffdea333e6976f2a6a1f3dbc4e
Igoru <igorsantos07@gmail.com>
2011-07-29 12:21:58 -0300

    adding admin page for currencies

--- admin/app.rb
+++ admin/app.rb
@@ -31,6 +31,7 @@ class Admin < Padrino::Application
   end
 
   access_control.roles_for :admin do |role|
+    role.project_module :currencies, "/currencies"
     role.project_module :users, "/users"
     role.project_module :accounts, "/accounts"
   end
---
+++ admin/controllers/currencies.rb
@@ -0,0 +1,47 @@
+Admin.controllers :currencies do
+
+  get :index do
+    @currencies = Currency.all
+    render 'currencies/index'
+  end
+
+  get :new do
+    @currency = Currency.new
+    render 'currencies/new'
+  end
+
+  post :create do
+    @currency = Currency.new(params[:currency])
+    if @currency.save
+      flash[:notice] = 'Currency was successfully created.'
+      redirect url(:currencies, :edit, :id => @currency.id)
+    else
+      render 'currencies/new'
+    end
+  end
+
+  get :edit, :with => :id do
+    @currency = Currency.find(params[:id])
+    render 'currencies/edit'
+  end
+
+  put :update, :with => :id do
+    @currency = Currency.find(params[:id])
+    if @currency.update_attributes(params[:currency])
+      flash[:notice] = 'Currency was successfully updated.'
+      redirect url(:currencies, :edit, :id => @currency.id)
+    else
+      render 'currencies/edit'
+    end
+  end
+
+  delete :destroy, :with => :id do
+    currency = Currency.find(params[:id])
+    if currency.destroy
+      flash[:notice] = 'Currency was successfully destroyed.'
+    else
+      flash[:error] = 'Impossible destroy Currency!'
+    end
+    redirect url(:currencies, :index)
+  end
+end
