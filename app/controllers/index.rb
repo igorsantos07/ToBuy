0ec161d06f35ec75dd309b4e0dab57b81775ea92
Igoru <igorsantos07@gmail.com>
2011-07-30 03:24:45 -0300

    late commit of home and login/logout pages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,25 @@
 ToBuy.controller :index do
-	get :index do
-		'hello world'
+
+  get :index, :map => '/' do
+    redirect url(:lists, :index)
+  end
+
+	get :login do
+		render '/index/login', nil, :layout => false
+	end
+
+	post :create_session, :map => '/create_session' do
+    if account = Account.authenticate(params[:email], params[:password])
+      set_current_account(account)
+      redirect '/'
+    else
+      flash[:warning] = "E-mail ou senha incorreto."
+      redirect url(:index, :login)
+    end
+	end
+
+	get :logout do
+    set_current_account(nil)
+    redirect url(:index, :login)
 	end
 end
