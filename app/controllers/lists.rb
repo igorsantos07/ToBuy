a53c49722442ed4de34d6d91b7442509e6e2ddc5
Igoru <igorsantos07@gmail.com>
2011-07-30 03:23:38 -0300

    spliting lists page in two: listing and form. updating sass and css and also adding a method to calculate total items for a list in its model

--- app/controllers/lists.rb
+++ app/controllers/lists.rb
@@ -1,15 +1,28 @@
 ToBuy.controllers :lists do
   get :index, :map => "/lists" do
     @lists = List.find_all_by_account_id current_account.id
-    render 'lists/index', :format => :html5
+    render 'lists/index'
   end
 
-  get :list, :with => :id do
+  get :view, :map => '/list/:id' do
     @list = List.find params[:id]
+    render 'lists/view'
+  end
+
+  get :new do
+    @currencies = Currency.order(:name).all
+    @currencies.collect! { |currency| [currency.symbol+' - '+currency.name, currency.id] }
+    render 'lists/new'
   end
 
   post :new do
-
+    @list = List.new(params[:list])
+    @list.account_id = current_account.id
+    if @list.save
+      redirect url(:lists, :view, :id => @list.id)
+    else
+      render 'lists/index'
+    end
   end
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ActiveRecord::Base
-	
+
+  def items_count
+    Item.count_by_sql "SELECT COUNT(*) FROM items WHERE list_id = #{self.id}"
+  end
+
 end
