d457d44d657c4b34edf597dc98310a2259fa8139
Igoru <igorsantos07@gmail.com>
2011-08-01 02:44:07 -0300

    to list pages: adding view page; making better use of routes; renaming rendered view folder

--- app/controllers/list.rb
+++ app/controllers/list.rb
@@ -1,28 +1,32 @@
-ToBuy.controllers :lists do
-  get :index, :map => "/lists" do
+ToBuy.controllers :list do
+  get :index do
     @lists = List.find_all_by_account_id current_account.id
-    render 'lists/index'
+    render 'list/index'
   end
 
-  get :view, :map => '/list/:id' do
+  get :view, :with => :id, :priority => :low do
     @list = List.find params[:id]
-    render 'lists/view'
+    render 'list/view'
   end
 
   get :new do
     @currencies = Currency.order(:name).all
     @currencies.collect! { |currency| [currency.symbol+' - '+currency.name, currency.id] }
-    render 'lists/new'
+    render 'list/new'
   end
 
   post :new do
     @list = List.new(params[:list])
     @list.account_id = current_account.id
     if @list.save
-      redirect url(:lists, :view, :id => @list.id)
+      redirect url(:list, :view, :id => @list.id)
     else
-      render 'lists/index'
+      render 'list/index'
     end
+  end
+
+  get :edit, :with => :id do
+
   end
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,13 @@
 class List < ActiveRecord::Base
 
+  def items
+    Item.find_all_by_list_id self.id
+  end
+
+  def currency
+    Currency.find self.currency_id
+  end
+
   def items_count
     Item.count_by_sql "SELECT COUNT(*) FROM items WHERE list_id = #{self.id}"
   end
