283291fd85c8b2bb9cd40429cc694d370a99cff4
Igoru <igorsantos07@gmail.com>
2011-08-06 02:23:44 -0300

    adding the total value of the list to the header

--- app/models/list.rb
+++ app/models/list.rb
@@ -12,4 +12,8 @@ class List < ActiveRecord::Base
     Item.count_by_sql "SELECT COUNT(*) FROM items WHERE list_id = #{self.id}"
   end
 
+  def total
+    items.collect { |i| i.price || 0 }.sum
+  end
+
 end
