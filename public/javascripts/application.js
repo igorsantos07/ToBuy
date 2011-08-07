1bd85cd098f3df77f7afa07839a6b66c184fd581
igoru <igorsantos07@gmail.com>
2011-08-07 14:58:15 -0300

    adding jquery to create confirm buttons and inactivate then when the confirm box is answered. closes #8

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,14 @@
-// Put your application scripts here
+$(document).ready(function() {
+
+  function mobile_confirm($btn, msg) {
+    $btn.click(function() {
+      var result = confirm(msg)
+      $btn.removeClass('ui-btn-active')
+      return result
+    })
+  }
+
+  mobile_confirm($('[data-feature=bought-btn]'), 'Deseja marcar essa lista como comprada?')
+  mobile_confirm($('[data-feature=delete-list-btn]'), 'Apagar mesmo esta lista?')
+
+})
