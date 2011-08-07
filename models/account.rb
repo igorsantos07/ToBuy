645477c4b37d3fe8f25d4f9945655c5b293ae6ff
Igoru <igorsantos07@gmail.com>
2011-08-07 13:59:04 -0300

    lowering the password's minimum length

--- models/account.rb
+++ models/account.rb
@@ -5,7 +5,7 @@ class Account < ActiveRecord::Base
   validates_presence_of     :email, :role
   validates_presence_of     :password,                   :if => :password_required
   validates_presence_of     :password_confirmation,      :if => :password_required
-  validates_length_of       :password, :within => 4..40, :if => :password_required
+  validates_length_of       :password, :within => 3..40, :if => :password_required
   validates_confirmation_of :password,                   :if => :password_required
   validates_length_of       :email,    :within => 3..100
   validates_uniqueness_of   :email,    :case_sensitive => false
