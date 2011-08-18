634b8e2c02d54e28595154bd6964ba4633dc08b3
Igoru <igorsantos07@gmail.com>
2011-08-18 02:44:29 -0300

    adding form to add new tracking codes; fixing little things in the tracking code listing; adding #url to tracking code model

--- app/controllers/tracking.rb
+++ app/controllers/tracking.rb
@@ -5,11 +5,17 @@ ToBuy.controllers :tracking, :parent => :list do
   end
 
   get :new do
-
+    @tracking_systems = TrackingSystem.all
+    render 'tracking/new'
   end
 
   post :new do
-
+    @tracking_code = TrackingCode.new(params[:tracking_code].merge! :list_id => params[:list_id])
+    if @tracking_code.save
+      redirect url(:tracking, :index, :list_id => params[:list_id])
+    else
+      render 'tracking/new'
+    end
   end
 
 end
--- app/models/tracking_code.rb
+++ app/models/tracking_code.rb
@@ -1,4 +1,14 @@
 class TrackingCode < ActiveRecord::Base
   belongs_to :tracking_system
 
+  before_save {|reg| reg.code.strip! }
+
+  def url
+    case tracking_system.method.upcase
+    when 'GET'
+      tracking_system.url % self[:code]
+    when 'POST'
+      '#'
+    end
+  end
 end
